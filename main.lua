dbcd7f31537fb66beeaa6fba9cb218ef59fd394b
Dan H <daniel.hernandez.dev@gmail.com>
2020-02-25 21:13:36 -0800

    add main game window

--- main.lua
+++ main.lua
@@ -1,15 +1,12 @@
--- Images on screen
 function love.load()
-   glowingImage = love.graphics.newImage("art/glowing.png")
+   menu = {"start", "options", "exit"}
+   for i=1,#menu do
+      print(menu[i])
+   end
 end
 
 function love.draw()
-   love.graphics.draw(glowingImage,100,100)
+   for i=1,#menu do
+      love.graphics.print(menu[i], 100, 100 + 50 * i)
+   end
 end
-
--- Play some audio
-fight = love.audio.newSource("sounds/filmfight.wav", "static") -- the "static" tells LÖVE to load the file into memory, good for short sound effects
-music = love.audio.newSource("sounds/epic.mp3", "stream") -- the "stream" tells LÖVE to stream the file from disk, good for longer music tracks
-
-fight:play()
-music:play()
