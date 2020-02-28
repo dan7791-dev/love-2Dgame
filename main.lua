8289d386652a21267a1d6640df0f45030b88a6fa
Daniel Hernandez <dehernandez7791@gmail.com>
2020-02-27 22:47:11 -0800

    add highlight to menu options

--- main.lua
+++ main.lua
@@ -1,12 +1,13 @@
 function love.load()
-   menu = {"start", "options", "exit"}
-   for i=1,#menu do
-      print(menu[i])
-   end
+   menu = {"start", "exit"}
 end
 
 function love.draw()
-   for i=1,#menu do
-      love.graphics.print(menu[i], 100, 100 + 50 * i)
-   end
+      love.graphics.print(menu[1], 100, 100 + 50 * 1)
+      love.graphics.print(menu[2], 100, 100 + 50 * 2)
+      if love.keyboard.isDown("up") then 
+         love.graphics.rectangle("line", 100, 100 + 50 * 1, 35, 25)
+      elseif love.keyboard.isDown("down") then
+         love.graphics.rectangle("line", 100, 100 + 50 * 2, 35, 25)
+      end
 end
