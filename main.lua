a58b3d05a67a0a1d0637fc0693bea8c337c6debf
Ryan Wojtyla <ryan@droplit.io>
2020-03-01 16:24:24 -0800

    added wagon

--- main.lua
+++ main.lua
@@ -21,21 +21,24 @@ function love.load()
    require("circle")
 
    -- initialize a new Rectangle
-   rect = Rectangle("line", 100, 100, 100, 200, 150)
+   --rect = Rectangle("line", 100, 100, 100, 200, 150)
  
    -- initialize a new Circle
-   circ = Circle("line", 100, 200, 100, 100)
+   --circ = Circle("line", 100, 200, 100, 100)
    
+   -- load the picture of a station wagon
+   wagonImg = love.graphics.newImage("wagon.png")
 end
 
 -- pretty output
 function love.draw()
-   rect:draw()
-   circ:draw()
+   --rect:draw()
+   --circ:draw()
+   love.graphics.draw(wagonImg, 0, 100, 0, 0.2, 0.2)
 end
 
 -- continually update variables
 function love.update(dt)
-   rect:update(dt)
-   circ:update(dt)
+   --rect:update(dt)
+   --circ:update(dt)
 end
