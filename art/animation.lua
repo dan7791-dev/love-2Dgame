a9afd061285d86cf4d054779a00f4818ef3b1526
Daniel Hernandez <dehernandez7791@gmail.com>
2020-03-02 21:30:56 -0800

    update art assets

--- art/animation.lua
+++ art/animation.lua
@@ -1,15 +1,15 @@
 function love.load()
    frames = {}
    
-   for i=1,5 do
-      table.insert(frames, love.graphics.newImage("art/jump/jump" .. i .. ".png"))
+   for i=1,7 do
+      table.insert(frames, love.graphics.newImage("art/Balloons/Balloons" .. i .. ".png"))
    end
    currentFrame = 1
 end
 
 function love.update(dt)
-   currentFrame = currentFrame + 10 * dt
-   if currentFrame >= 6 then
+   currentFrame = currentFrame + 15 * dt
+   if currentFrame >= 8 then
       currentFrame = 1
    end
 end
--- sounds/music.lua
+++ sounds/music.lua
@@ -1,6 +1,4 @@
 -- Play some audio
-fight = love.audio.newSource("sounds/filmfight.wav", "static") -- the "static" tells LÖVE to load the file into memory, good for short sound effects
 music = love.audio.newSource("sounds/epic.mp3", "stream") -- the "stream" tells LÖVE to stream the file from disk, good for longer music tracks
 
-fight:play()
 music:play()
