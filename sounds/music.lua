d7abc19a949eb7d32e9b021a48fe9533254c9e35
Daniel Hernandez <dehernandez7791@gmail.com>
2020-03-03 22:42:05 -0800

    add angle movement

--- sounds/music.lua
+++ sounds/music.lua
@@ -1,4 +1,4 @@
 -- Play some audio
-music = love.audio.newSource("sounds/epic.mp3", "stream") -- the "stream" tells LÖVE to stream the file from disk, good for longer music tracks
+music = love.audio.newSource("up.mp3", "stream") -- the "stream" tells LÖVE to stream the file from disk, good for longer music tracks
 
 music:play()
